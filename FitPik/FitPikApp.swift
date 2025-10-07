2535bf0ea006e18eeba1a5707e3704641e50b674
randymesso <Randymesso@gmail.com>
2025-10-07 17:12:45 -0500

    Initial Commit

---
+++ FitPik/FitPikApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitPikApp.swift
+//  FitPik
+//
+//  Created by Randy Messo on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FitPikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
